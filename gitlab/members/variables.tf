7acce24706a8d0e8b9d3c1467e15dc39175d3a89
hutger <hutger@gmail.com>
2022-11-08 15:33:03 +0100

    Updating variable descriptions.

--- gitlab/members/variables.tf
+++ gitlab/members/variables.tf
@@ -1,21 +1,26 @@
 variable "member_name" {
-  type = string
+  type        = string
+  description = "GitLab user name"
 }
 
 variable "member_project" {
-  type = string
+  type        = string
+  description = "Project ID full path"
 }
 
 variable "member_access_level" {
-  type = string
+  type        = string
+  description = "The access level for the member. Valid values are: no one, minimal, guest, reporter, developer, maintainer"
 }
 
 variable "member_expires_at" {
-  type    = string
-  default = null
+  type        = string
+  default     = null
+  description = "Expiration date for the project membership. Format: YYYY-MM-DD"
 }
 
 variable "member_description" {
-  type    = string
-  default = "null"
+  type        = string
+  default     = "null"
+  description = "Membership description"
 }
--- gitlab/projects/variables.tf
+++ gitlab/projects/variables.tf
@@ -1,5 +1,6 @@
 variable "proj_name" {
-  type = string
+  type        = string
+  description = "The name of the project. 15 chars limit"
   validation {
     condition     = length(var.proj_name) <= 15
     error_message = "Names cannot have more than 15 chars of length"
@@ -7,89 +8,107 @@ variable "proj_name" {
 }
 
 variable "proj_description" {
-  type = string
+  type        = string
+  description = "A description of the project."
 }
 
 variable "proj_namespace" {
-  type = string
+  type        = string
+  description = "The namespace (group or user) of the project."
 }
 
 variable "proj_default_branch" {
-  type    = string
-  default = "main"
+  type        = string
+  default     = "main"
+  description = "The default branch for the project."
 }
 
 variable "proj_mr_approvals_required" {
-  type    = number
-  default = 0
+  type        = number
+  default     = 0
+  description = "The number of approvals required for this rule."
 }
 
 variable "proj_path" {
-  type    = string
-  default = null
+  type        = string
+  default     = null
+  description = "The path of the repository."
 }
 
 variable "proj_visibility_level" {
-  type    = string
-  default = "private"
+  type        = string
+  default     = "private"
+  description = " Set to public to create a public project."
 }
 
 variable "proj_push_access_level" {
-  type    = string
-  default = "maintainer"
+  type        = string
+  default     = "maintainer"
+  description = "Access levels allowed to push. Valid values are: no one, developer, maintainer."
 }
 
 variable "proj_merge_access_level" {
-  type    = string
-  default = "maintainer"
+  type        = string
+  default     = "maintainer"
+  description = "Access levels allowed to merge. Valid values are: no one, developer, maintainer."
 }
 
 variable "proj_unprotect_access_level" {
-  type    = string
-  default = "maintainer"
+  type        = string
+  default     = "maintainer"
+  description = "Access levels allowed to unprotect. Valid values are: developer, maintainer."
 }
 
 variable "proj_only_allow_merge_if_pipeline_succeeds" {
-  type    = bool
-  default = true
+  type        = bool
+  default     = true
+  description = "Set to true if you want allow merges only if a pipeline succeeds."
 }
 
 variable "proj_allow_merge_on_skipped_pipeline" {
-  type    = bool
-  default = true
+  type        = bool
+  default     = true
+  description = "Set to true if you want to treat skipped pipelines as if they finished with success."
 }
 
 variable "proj_container_registry_enabled" {
-  type    = bool
-  default = true
+  type        = bool
+  default     = true
+  description = "Enable container registry for the project."
 }
 
 variable "proj_container_expiration_enabled" {
-  type    = bool
-  default = true
+  type        = bool
+  default     = true
+  description = "If true, the policy is enabled."
 }
 
 variable "proj_remove_source_branch_after_merge" {
-  type    = bool
-  default = true
+  type        = bool
+  default     = true
+  description = "Enable Delete source branch option by default for all new merge requests."
 }
 
 variable "proj_packages_enabled" {
-  type    = bool
-  default = true
+  type        = bool
+  default     = true
+  description = "Enable packages repository for the project."
 }
 
 variable "proj_pipelines_enabled" {
-  type    = bool
-  default = true
+  type        = bool
+  default     = true
+  description = "Enable pipelines for the project."
 }
 
 variable "proj_snippets_enabled" {
-  type    = bool
-  default = true
+  type        = bool
+  default     = true
+  description = "Enable snippets for the project."
 }
 
 variable "proj_wiki_enabled" {
-  type    = bool
-  default = true
+  type        = bool
+  default     = true
+  description = "Enable wiki for the project."
 }
